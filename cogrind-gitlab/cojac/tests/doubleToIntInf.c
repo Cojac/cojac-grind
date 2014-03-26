e8744edf0aa7c67b8e3b849650b7c0813e621abb
Luis Domingues <domigues.luis@gmail.com>
2014-03-26 22:01:12 +0100

    Add float casts to int tests.

---
+++ cogrind-gitlab/cojac/tests/doubleToIntInf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+
+int main(){
+	double a = 123.25 / 0;
+	int b;
+	b = a;
+	return 0;
+}
---
+++ cogrind-gitlab/cojac/tests/doubleToLongOverflow.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+
+int main(){
+	double a = LONG_MAX * 2.0;
+	long b;
+	b = a;
+	return 0;
+}
